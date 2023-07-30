7248b8cfccab458c94944e92b523e595689e7cc5
Ryan <63191069+Rainyryan@users.noreply.github.com>
2023-07-31 05:04:34 +0800

    test tts

--- local_lens_app/lib/main.dart
+++ local_lens_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart'; // Flutter material design library
+import 'package:local_lens_app/tts_test.dart';
 import 'google_ml_kit_text_detection/text_detector_view.dart';
+import 'tts_test.dart';
 
 Future<void> main() async {
   runApp(MyApp());
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TextRecognizerView(), // Set Home widget as the home of the app
+      // home: TextRecognizerView(), // Set Home widget as the home of the app
+      home: MyHomePage(),
     );
   }
 }
